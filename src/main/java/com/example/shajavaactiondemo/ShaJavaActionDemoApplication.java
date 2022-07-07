8033f64bfe74aee2026e8cf242bf168d8f77f9ff
Senol Atac <senol.atac.1@gmail.com>
2022-07-07 11:51:03 +0300

    initial

---
+++ src/main/java/com/example/shajavaactiondemo/ShaJavaActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shajavaactiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaJavaActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShaJavaActionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shajavaactiondemo/ShaJavaActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shajavaactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaJavaActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
